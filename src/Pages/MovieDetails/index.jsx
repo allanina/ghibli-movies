36f71f8ffc749dc43e8ec48f1b08b80a441bd914
Allana Mendes <allana.mendes@INSOLE.LOCAL>
2022-01-20 16:56:56 -0300

    added return button to the movie page

--- src/Pages/MovieDetails/index.jsx
+++ src/Pages/MovieDetails/index.jsx
@@ -1,23 +1,28 @@
+import { Rating } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 
 function MovieDetails() {
   const { id } = useParams();
-const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-console.log(movies)
+  console.log(movies)
 
-useEffect(() => {
-  fetch(`https://ghibliapi.herokuapp.com/films/${id}`)
+  useEffect(() => {
+    fetch(`https://ghibliapi.herokuapp.com/films/${id}`)
       .then(response => response.json())
       .then(data => setMovies(data))
-}, [])
+  }, [])
 
   return (
-      <div>
-        <img style={{width: '50vh'}} src={movies.image} alt="banner" />
+    <div>
+      <div >
+        <Link to="/">
+          <button>Voltar</button>
+        </Link>
       </div>
+    </div>
   )
 }
 
